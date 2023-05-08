c896a168b66e9a646e25f5f40f0d0cf7c012c90e
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-08 16:36:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
 function HomePage() {
   return <div>Welcome to Next.js!</div>
 }
 
 export default HomePage
+=======
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import Banner from "../components/banner";
+export default function Home() {
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
+      </main>
+    </div>
+  );
+>>>>>>> 7f42296 (coffee_hub)
