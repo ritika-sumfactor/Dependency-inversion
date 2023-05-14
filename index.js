b79110cda7c1cfde6c500bc83182e4303306039f
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-14 23:34:00 +0530

    Pages CSR

--- index.js
+++ index.js
@@ -1,83 +1,27 @@
-import coffeeStoresData from "../data/coffee-stores.json";
-import { fetchCoffeeStores } from "../lib/coffee-stores";
-
-import useTrackLocation from "../hooks/use-track-location";
-
-export async function getStaticProps(context) {
-  console.log("hi getStaticProps");
-
-export async function getStaticProps(context) {
+import { useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 export default function Home(props) {
-  console.log("props", props);
-
-  const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
-    useTrackLocation();
 
   console.log({ latLong, locationErrorMsg });
 
+  useEffect(() => {
+    async function setCoffeeStoresByLocation() {
+      if (latLong) {
+        try {
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 6);
+          console.log({ fetchedCoffeeStores });
+          //set coffee stores
+        } catch (error) {
+          //set error
+          console.log("Error", { error });
+        }
+      }
+    }
+    setCoffeeStoresByLocation();
+  }, [latLong]);
+
   const handleOnBannerBtnClick = () => {
     console.log("hi banner button");
-    // handleTrackLocation();
     handleTrackLocation();
-  };
-  return (
-    <div className={styles.container}>
- export default function Home(props) {
-
-      <main className={styles.main}>
-        <Banner
-          buttonText="View stores nearby"
-          buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
-          handleOnClick={handleOnBannerBtnClick}
-        />
-        {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
-        <div className={styles.heroImage}>
-          <Image src="/static/hero-image.png" width={700} height={400} />
-        </div>
-        {props.coffeeStores.length > 0 && (
-          <>
-            <h2 className={styles.heading2}>Toronto stores</h2>
-            <div className={styles.cardLayout}>
-              {props.coffeeStores.map((coffeeStore) => {
-                return (
-                  <Card
-                    key={coffeeStore.id}
-                    name={coffeeStore.name}
-                    imgUrl={
-                      coffeeStore.imgUrl ||
-                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-                    }
-                    href={`/coffee-store/${coffeeStore.id}`}
-                    className={styles.card}
-                  />
-                );
-              })}
-            </div>
-          </>
-        )}
-        <div className={styles.sectionWrapper}>
-          {props.coffeeStores.length > 0 && (
-            <>
-              <h2 className={styles.heading2}>Toronto stores</h2>
-              <div className={styles.cardLayout}>
-                {props.coffeeStores.map((coffeeStore) => {
-                  return (
-                    <Card
-                      key={coffeeStore.id}
-                      name={coffeeStore.name}
-                      imgUrl={
-                        coffeeStore.imgUrl ||
-                        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-                      }
-                      href={`/coffee-store/${coffeeStore.id}`}
-                      className={styles.card}
-                    />
-                  );
-                })}
-              </div>
-            </>
-          )}
-        </div>
-      </main>
-    </div>
-  );
