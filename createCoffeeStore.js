02e6783a745847de814860c96785e28b0c06cb50
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-17 16:39:01 +0530

    Delete createCoffeeStore.js

--- createCoffeeStore.js
+++
@@ -1,6 +0,0 @@
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
-);
-
-const table = base("coffee-stores");
