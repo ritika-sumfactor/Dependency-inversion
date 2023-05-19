cfdd82941281df53eed313821fbe03d4b43539dd
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-19 12:57:38 +0530

    pages

---
+++ createcoffeestore.js
@@ -0,0 +1,59 @@
+import { table, getMinifiedRecords } from "../../lib/airtable";
+import {
+  table,
+  getMinifiedRecords,
+  findRecordByFilter,
+} from "../../lib/airtable";
+
+const createCoffeeStore = async (req, res) => {
+  if (req.method === "POST") {
+    //find a record
+    const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+
+    try {
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id="${id}"`,
+          })
+          .firstPage();
+        const records = await findRecordByFilter(id);
+
+        if (findCoffeeStoreRecords.length !== 0) {
+          const records = getMinifiedRecords(findCoffeeStoreRecords);
+        if (records.length !== 0) {
+          res.json(records);
+        } else {
+          //create a record
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+            const records = getMinifiedRecords(createRecords);
+            res.json(records);
+          } else {
+            res.status(400);
+            res.json({ message: "Id or name is missing" });
+          }
+        }
+      } else {
+        res.status(400);
+        res.json({ message: "Id is missing" });
+      }
+    } catch (err) {
+      console.error("Error creating or finding a store", err);
+      res.status(500);
+      res.json({ message: "Error creating or finding a store", err });
+    }
+  }
+};
+export default createCoffeeStore;
