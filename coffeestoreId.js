658a47764c70580db985f8b28099ae55c596c252
ritika-sumfactor <129729731+ritika-sumfactor@users.noreply.github.com>
2023-05-19 16:17:31 +0530

    API

--- coffeestoreId.js
+++ coffeestoreId.js
@@ -1,15 +1,35 @@
 import { findRecordByFilter } from "../../lib/airtable";
+import { table, findRecordByFilter } from "../../lib/airtable";
 
 const favouriteCoffeeStoreById = async (req, res) => {
   if (req.method === "PUT") {
     try {
       const { id } = req.body;
-
       if (id) {
         const records = await findRecordByFilter(id);
 
         if (records.length !== 0) {
           res.json(records);
+          const record = records[0];
+
+          const calculateVoting = parseInt(record.voting) + 1;
+
+          console.log({ calculateVoting });
+
+          // update a record
+
+          const updateRecord = await table.update([
+            {
+              id: record.id,
+              fields: {
+                voting: calculateVoting,
+              },
+            },
+          ]);
+
+          if (updateRecord) {
+            res.json(updateRecord);
+          }
         } else {
           res.json({ message: "Coffee store id doesn't exist", id });
         }
@@ -23,5 +43,4 @@ const favouriteCoffeeStoreById = async (req, res) => {
     }
   }
 };
-
 export default favouriteCoffeeStoreById;
